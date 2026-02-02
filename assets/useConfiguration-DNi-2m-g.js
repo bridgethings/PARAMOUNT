333418a852b749b0fec16b8fc1fc86a51126d4ca
venugopalreddysomu <51040807+venugopalreddysomu@users.noreply.github.com>
2026-02-02 16:20:21 +0530

    initial commit

---
+++ assets/useConfiguration-DNi-2m-g.js
@@ -0,0 +1,6 @@
+import{s as t,w as n,ab as a}from"./index-Dtok5lqr.js";/**
+ * @license @tabler/icons-react v3.29.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var r=t("outline","download","IconDownload",[["path",{d:"M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2",key:"svg-0"}],["path",{d:"M7 11l5 5l5 -5",key:"svg-1"}],["path",{d:"M12 4l0 12",key:"svg-2"}]]);const s=()=>{const o=n.useContext(a);if(!o)throw new Error("useConfiguration must be used within a ConfigurationProvider");return o};export{r as I,s as u};
